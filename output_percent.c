106e84c4f688eb99e31c542eb5d23c6ff23c4223
Tycee02 <wakilaminat@gmail.com>
2023-10-17 12:51:20 -0700

    Write a function that produces output according to a format %

---
+++ output_percent.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * output_percent - This function prints the percent sign.
+ *
+ * Return: should return 1.
+ */
+int output_percent(void)
+{
+	_putchar(37);
+	return (1);
+}
